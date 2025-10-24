3fe47261d05e2a80f395405e0d78a153fb1a8ddf
barbaro <barbaro.martinez@gmail.com>
2025-10-24 15:26:11 -0300

    First commit

---
+++ Models/UserDTO.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OficiosUy.Api.Models;
+
+public class UserDTO
+{
+    
+    [Required(ErrorMessage = "First Name is a required field")]
+    [MaxLength(50)]
+    public required String FirstName { get; set; }
+    
+    [Required(ErrorMessage = "LastName is a required field")]
+    [MaxLength(50)]
+    public required String LastName { get; set; }
+    
+    [MaxLength(100)]
+    [EmailAddress]
+    [Required(ErrorMessage = "Email is a required field")]
+    public required String Email { get; set; }
+    
+    [MaxLength(50)]
+    [Required(ErrorMessage = "Phone is a required field")]
+    public required String Phone { get; set; }
+}
